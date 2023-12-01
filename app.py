158904c3cd70992b17866101b37fedca55664589
Sasson Margaliot <sasson@live.com>
2023-12-01 14:29:43 +0200

    stonealgo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dialog_page import DialogPage
 
 page_name = "app"
-page_channel = "simple.wikipedia.org"
+page_channel = "StoneAlgo.com"
 
 base_url = "https://sasson-dialogexpress-app-jkbb2w.streamlit.app"
 DialogPage.start(page_name = page_name, page_channel = page_channel)
