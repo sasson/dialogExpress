b52b03b3d4d9d482d05aa3276603af27d295d396
Sasson Margaliot <sasson@live.com>
2023-12-06 15:47:51 +0200

    article

--- dialog_page.py
+++ dialog_page.py
@@ -130,9 +130,9 @@ class DialogPage():
     
     def render_chatbot_message(self, generated_content : str):
         oText = Text(generated_content = generated_content, concepts = self.agent.concepts)
-        st.markdown(self.generate_html_for_results(), unsafe_allow_html=True)
-        st.markdown("<br />", unsafe_allow_html=True)
         st.markdown(self.generate_html_for_answer(oText=oText), unsafe_allow_html=True)
+        st.markdown("<br />", unsafe_allow_html=True)
+        st.markdown(self.generate_html_for_results(), unsafe_allow_html=True)
 
     def render_message(self, message_object):
         # message["role"] is used because we need to identify user and bot
